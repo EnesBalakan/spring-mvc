bb71cc2a2a7665a7d7a90d67853ea0d47672cdb9
Enes Balakan <redenesbalakan@gmail.com>
2025-04-02 01:06:53 +0300

    Adding Data to Spring MVC Model
    three versions of adding data to spring mvc model, writing hello "name" to viewPage

--- 01-thymeleafdemo-helloWorld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 01-thymeleafdemo-helloWorld/src/main/java/com/learning/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.learning.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,43 @@ public class HelloWorldController {
     //need a controller method to  process the HTML form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloWorld";
+    }
+
+    // need a controller method to  read form data and
+    // add data to the model
+
+    @RequestMapping("/processFormTwo")
+    public String letsBuyCar(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps (tüm harfleri büyük harfe çevir)
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message",result);
+
+        return "helloWorld";
+    }
+
+    @RequestMapping("/processFormThree")
+    public String processFormThree(@RequestParam("studentName") String theName, Model model){
+
+
+        // convert the data to all caps (tüm harfleri büyük harfe çevir)
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my Friend from v3!  " + theName;
+
+        // add message to the model
+        model.addAttribute("message2",result);
+
         return "helloWorld";
     }
 }
